6d30551611bb865e3c9304f269e3fbf5f7e8bc30
L1v1T <Chenyq@mail.ustc.edu.cn>
2019-05-20 14:38:05 +0800

    Update timer

--- demo.py
+++ demo.py
@@ -1,19 +1,26 @@
 from paillier import *
+import time
 
 print("Generating key pair...")
 n, g, l, m = keyGen(2048)
 print("Generated key pair.")
-plaintext1 = 1234
-plaintext2 = 4321
+plaintext1 = 1234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234
+plaintext2 = 4321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321432143214321
+tstart = time.time()
 c1 = encrypt(plaintext1, n, g)
 c2 = encrypt(plaintext2, n, g)
+tend = time.time()
 print("c1: " + str(c1))
 print("c2: " + str(c2))
+print("average time: " + str((tend - tstart) / 2))
 c_ = plaintextAdd(c1, c2, n, g)
-print("c_: " + str(c_))
+tstart = time.time()
 m1 = decrypt(c1, n, g, l, m)
-print("m1: " + str(m1))
 m2 = decrypt(c2, n, g, l, m)
-print("m2: " + str(m2))
 m_bar = decrypt(c_, n, g, l, m)
+tend = time.time()
+print("c_: " + str(c_))
+print("m1: " + str(m1))
+print("m2: " + str(m2))
 print("m_bar: " + str(m_bar))
+print("average time: " + str((tend - tstart) / 3))
